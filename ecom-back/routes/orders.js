ce67e4b4aa3837b91e5cca346427989fc144e871
okwueze <69356352+okwueze@users.noreply.github.com>
2020-11-03 15:08:49 +0100

    jjuu

--- ecom-back/routes/orders.js
+++ ecom-back/routes/orders.js
@@ -32,4 +32,50 @@ router.get('/', (req, res) => {
 
 })
 
+/* GET SINGLE ORDERS */
+router.get('/:id', (req, res) => {
+  const orderId = req.params.id;
+  
+  database.table('orders_details as od')
+    .join([
+      {
+        table: 'orders as o',
+        on: 'o.id = od.order_id'
+      },
+      {
+        table: 'products as p',
+        on: 'p.id = od.product_id',
+      },
+      {
+        table: 'users as u',
+        on: 'u.id = o.user_id'
+      }
+    ])
+    .withFields(['o.id', 'p.title as name', 'p.description', 'p.price', 'u.username'])
+    .filter({'o.id': orderId})
+    .getAll() // Don't forget the .getAll() or .get() must come after all functions but before the .then() function
+    .then(orders => {
+      if (orders.length > 0) {
+        res.status(200).json(orders);
+      } else {
+        res.json({message: `No orders found with orderId ${orderId}`});
+      }
+    }).catch(err => console.log(err));
+})
+
+
+/* PLACE A NEW ORDER */
+router.post('/new', (req, res) => {
+  
+  let {userId, products} = req.body;
+
+  console.log(userId, products)
+
+
+
+
+
+
+})
+
 module.exports = router;
